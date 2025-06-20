832313882a6422da4bd0b737b531d8f517c5c48e
Clifton J. Patrick III <cliftunpatricius.git@proton.me>
2025-06-19 22:38:20 -0500

    Refactor status_bar.sh for better macOS support:
    * Use OpenBSD commands on OpenBSD and Darwin commands on Darwin
    * Add CPU count stat

--- bin/status_bar.sh
+++ bin/status_bar.sh
@@ -21,13 +21,15 @@ readonly timestamp
 # Hardware
 #
 
-cpu_temp="$(sysctl hw.sensors.cpu0.temp0 | cut -d "=" -f 2 | cut -d " " -f 1)"
-cpu_speed="$(sysctl hw.cpuspeed | awk -F '=' '{print $2}')"
-
-mem_free="$(top -n | grep Memory | awk '{print $6}')"
+cpu_count="$(sysctl hw.ncpu | awk '{print $2}')"
 
 if test "${ME_OPERATING_SYSTEM}" = "OpenBSD"
 then
+	cpu_temp="$(sysctl hw.sensors.cpu0.temp0 | cut -d "=" -f 2 | cut -d " " -f 1)"
+	cpu_speed="$(sysctl hw.cpuspeed | awk -F '=' '{print $2}')"
+
+	mem_free="$(top -n | grep Memory | awk '{print $6}')"
+
 	battery_percentage_remaining="$(apm -l)"
 	battery_charging_indicator="$(sysctl hw.sensors.acpiac0.indicator0 | grep -c On)"
 
@@ -42,6 +44,11 @@ then
 	fi
 elif test "${ME_OPERATING_SYSTEM}" = "Darwin"
 then
+	cpu_temp=""
+	cpu_speed=""
+
+	mem_free="$(top -l 1 | grep PhysMem | awk '{print $8}')"
+
 	battery_percentage_remaining="$(ioreg -c AppleSmartBattery | grep '"CurrentCapacity"' | awk -F ' = ' '{print $2}')"
 	battery_charging_indicator="$(ioreg -c AppleSmartBattery | grep '"ChargerConfiguration"' | awk -F ' = ' '{print $2}')"
 
@@ -56,7 +63,7 @@ then
 	fi
 fi
 
-readonly cpu_temp cpu_speed \
+readonly cpu_count cpu_temp cpu_speed \
 	mem_free \
 	battery_percentage_remaining battery_charging_indicator battery_charging_comment
 
@@ -64,20 +71,28 @@ readonly cpu_temp cpu_speed \
 # Network
 #
 
-ssid="$(ifconfig | grep join | sed -En 's/^.*join[[:space:]]+([[:alnum:]]+)[[:space:]]+chan.*$/\1/p')"
-iface="$(route -n show | grep default | awk '{print $8}')"
-private_ip="$(ifconfig "${iface}" | grep inet | awk '{print $2}')"
-public_ip="$(curl -s https://ifconfig.me)"
+if test "${ME_OPERATING_SYSTEM}" = "OpenBSD"
+then
+	iface="$(route -n show | grep default | awk '{print $8}')"
+	ssid="$(ifconfig "${iface}" | grep join | sed -En 's/^.*join[[:space:]]+([[:alnum:]]+)[[:space:]]+chan.*$/\1/p')"
+elif test "${ME_OPERATING_SYSTEM}" = "Darwin"
+then
+	iface="$(route -n get default | grep -E '^[[:space:]]+interface:[[:space:]]+' | awk '{print $2}')"
+	ssid=""
+fi
 
-readonly ssid iface private_ip public_ip
+public_ip="$(curl -s https://ifconfig.me)"
+private_ip="$(ifconfig "${iface}" | grep -E '^[[:space:]]+inet[[:space:]]+[[:digit:]]+' | awk '{print $2}')"
+
+readonly iface ssid public_ip private_ip
 
 #
 # Status Bar
 #
 
-printf 'battery: %s (%s) | cpu: %s (%sMHz) | mem_free: %s | network: %s / %s (%s) | %s\n' \
+printf 'bat: %s (%s) | cpu: %s (%s at %sMHz) | mem: %s | net: %s / %s (%s) | %s\n' \
 	"${battery_percentage_remaining}${percent_symbol}" "${battery_charging_comment}" \
-	"${cpu_temp}${degrees_celsius_symbol}" "${cpu_speed}" \
+	"${cpu_count}" "${cpu_temp}${degrees_celsius_symbol}" "${cpu_speed}" \
 	"${mem_free}" \
 	"${public_ip}" "${private_ip}" "${ssid}" \
 	"${timestamp}"
